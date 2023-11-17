657809a4f1cbd7c30db900b5995f3eb7b49a54c9
Tauhid Patel <tauhidpatel18@gmail.com>
2023-11-17 21:53:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'accordion-with-search';
+  
+  displayStyle = "none";
+  
+  @ViewChild('accordionSearchBar') accordionSearchBar!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.initializeSearch();
+  }
+
+  initializeSearch(): void {
+    const containsCaseInsensitive = (n: string, m: string) => {
+      return n.toUpperCase().includes(m.toUpperCase());
+    };
+
+    const accordionSearchBar = this.accordionSearchBar.nativeElement as HTMLInputElement;
+    const cardHeaders = document.querySelectorAll('.card-header');
+
+    this.renderer.listen(accordionSearchBar, 'input', () => {
+      const searchTerm = accordionSearchBar.value;
+
+      cardHeaders.forEach(header => {
+        const headerElement = header as HTMLElement;
+        const headerContent = headerElement.textContent || '';
+        if (containsCaseInsensitive(headerContent, searchTerm)) {
+          headerElement.style.display = 'block';
+        } else {
+          headerElement.style.display = 'none';
+        }
+      });
+
+      const panels = document.querySelectorAll('#accordion .accordion-item');
+      
+      panels.forEach(panel => {
+        const panelElement = panel as HTMLElement;
+        const panelContent = panelElement.textContent || '';
+        if (!containsCaseInsensitive(panelContent, searchTerm)) {
+          panelElement.style.display = 'none';
+        } else {
+          panelElement.style.display = 'block';
+        }
+      });
+
+      
+
+    });
+  }
+ 
+  openPopup() {
+    this.displayStyle = "block";
+  }
+
+  closePopup() {
+    this.displayStyle = "none";
+  }
+
+  ngOnInit(): void { }
+
 }
